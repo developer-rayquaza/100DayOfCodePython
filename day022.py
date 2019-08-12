120e02dad4a02f3b576137321caf9225a156afc1
Sanjay Sanju <sanjaybabu618@gmail.com>
2019-08-12 12:31:56 +0530

    JugsMugsPugsPlus Reverse

---
+++ day022.py
@@ -0,0 +1,15 @@
+in_put = int(input())
+rev = int(input())
+
+out_put = ""
+
+if(rev):
+  if(in_put%7 is 0 or '7' in str(in_put)): out_put += "Pugs"
+  if(in_put%5 is 0 or '5' in str(in_put)): out_put += "Mugs"
+  if(in_put%3 is 0 or '3' in str(in_put)): out_put += "Jugs"
+else:
+  if(in_put%3 is 0 or '3' in str(in_put)): out_put += "Jugs"
+  if(in_put%5 is 0 or '5' in str(in_put)): out_put += "Mugs"
+  if(in_put%7 is 0 or '7' in str(in_put)): out_put += "Pugs"
+
+print(out_put or in_put)
